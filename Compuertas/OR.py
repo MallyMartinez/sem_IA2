2e88b86e1dcdf05b86c5887eb702a4833532b413
MallyMartinez <mally.hernandez2861@alumnos.udg.mx>
2024-08-20 22:48:10 -0600

    Compuestas
    AND y OR

---
+++ Compuertas/OR.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self, input_size, learning_rate=0.1, epochs=10000):
+        self.W = np.zeros(input_size + 1) 
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+
+    def activation_function(self,x):
+        return 1 if x >= 0 else 0
+
+    def predict(self, x):
+        x = np.insert(x, 0, 1)
+        z = self.W.T.dot(x)
+        return self.activation_function(z)
+
+    def train(self, X, y):
+        for _ in range(self.epochs):
+            for inputs, label in zip(X,y):
+                prediction = self.predict(inputs)
+                self.W += self.learning_rate * (label - prediction) *np.insert(inputs, 0, 1)
+
+    def print_weights(self):
+        print(f"Pesos despues del entrenamiento: {self.W}")
+
+X= np.array([
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+])
+
+y=np.array([0, 1, 1, 1])
+
+perceptron = Perceptron(input_size=2)
+perceptron.train(X,y)
+
+perceptron.print_weights()
+
+for inputs in X:
+    print(f"Entrada: {inputs} -> Salida predicha: {perceptron.predict(inputs)}")
