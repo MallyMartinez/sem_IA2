6417f0e729b359a8597fe7078df12f564053cd41
MallyMartinez <mallymartinez20@gmail.com>
2024-09-23 01:13:36 -0600

    Rename "mlp.py" to "mlp_minist.py" and create file "mlp_speeches.py"

---
+++ Perceptron_Multicapa/mlp_minist.py
@@ -0,0 +1,7 @@
+"""
+Practica 1: Perceptrón Multicapa
+Mally Samira Hernandez Martinez | Código: 220286113 
+Ingenieria de computacion (INCO) | Seccion D05 I7041
+Seminario de Solucion de Problemas de Inteligencia Artificial II
+"""
+
